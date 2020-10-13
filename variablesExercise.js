3f852e8e3e7a8595982c407ce09398a7ce7aa0d4
Matthew RIley <matthewriley@Matthews-MacBook-Pro.local>
2020-10-13 14:02:05 -0500

    Completed Exercise

---
+++ variablesExercise.js
@@ -0,0 +1,19 @@
+let myAge = 33;
+
+myAge = 28;
+
+// myAge = myAge + 6;
+myAge += 6;
+
+myAge--;
+
+myAge = "None of your business";
+
+console.log(myAge);
+
+let blank;
+blank = null;
+
+// Bonus
+let notANumber = 0 / 0;
+console.log(notANumber);
